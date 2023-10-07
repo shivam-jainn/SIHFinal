1f707e98b2ffd4955988e3b2628cab1280ac5b82
shivam-jainn <shivam.clgstash@gmail.com>
2023-10-07 17:22:45 +0530

    addProject button

---
+++ bricfront/components/Addproject/AddProject.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@mantine/core"; 
+
+
+export default function AddProject() {
+  return (
+    <Button rightSection={<IconDownload size={14} />}>Download</Button>
+  )
+}
