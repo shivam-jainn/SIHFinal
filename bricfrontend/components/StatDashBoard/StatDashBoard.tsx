9649e662249df1c95f1b96b89c8634f60115ae4b
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-10-27 23:30:30 +0530

    fetching data using socket.io

--- bricfrontend/components/StatDashBoard/StatDashBoard.tsx
+++ bricfrontend/components/StatDashBoard/StatDashBoard.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import { Line } from 'react-chartjs-2';
 
-// Define an interface for the data structure
-interface MachineData {
+type MachineStats = {
     Machine_ID: string;
     Timestamp: string;
     Machine_Temperature_C: number;
@@ -11,80 +8,52 @@ interface MachineData {
     Vibration_mm_s2: number;
     Environment_Temperature_C: number;
     Humidity: number;
-}
+};
 
-export default function StatDashBoard() {
-    const [machineStats, setMachineStats] = useState<MachineData>({
-        Machine_ID: '',
-        Timestamp: '',
-        Machine_Temperature_C: 0,
-        Soundwave_dB: 0,
-        Vibration_mm_s2: 0,
-        Environment_Temperature_C: 0,
-        Humidity: 0,
-    });
-
-    const [temperatureData, setTemperatureData] = useState<number[]>([]);
-    const [timestamps, setTimestamps] = useState<string[]>([]);
+const Dash: React.FC = () => {
+    const [machineStats, setMachineStats] = useState<MachineStats[]>([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        // Function to update data and add it to the chart
-        const updateData = (data: MachineData) => {
-            setMachineStats(data);
-
-            // Update temperature data and timestamps for the chart
-            setTemperatureData((prevData) => [...prevData, data.Machine_Temperature_C]);
-            setTimestamps((prevTimestamps) => [...prevTimestamps, data.Timestamp]);
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/data.json'); // Assuming the JSON file is in your public directory
+                const data: MachineStats[] = await response.json();
+                setMachineStats(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
 
-        // Fetch data from data.json (for initial data)
-        fetch('/data.json') // Adjust the path as needed
-            .then((response) => response.json())
-            .then((data: MachineData) => {
-                updateData(data);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
+        // Set up an interval to refresh data every 5 seconds
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % machineStats.length);
+        }, 5000);
 
-        const socket = io('http://localhost:3000'); // Adjust the URL to match your server
-
-        socket.on('machine-stats', (data: MachineData) => {
-            updateData(data);
-        });
+        // Fetch data initially and clear the interval when the component unmounts
+        fetchData();
 
         return () => {
-            socket.disconnect();
+            clearInterval(interval);
         };
-    }, []);
-
-    const chartData = {
-        labels: timestamps,
-        datasets: [
-            {
-                label: 'Machine Temperature (°C)',
-                data: temperatureData,
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-            },
-        ],
-    };
+    }, [machineStats]);
 
     return (
         <div>
             <h1>Machine Statistics</h1>
-            <ul>
-                <li>Machine ID: {machineStats.Machine_ID}</li>
-                <li>Timestamp: {machineStats.Timestamp}</li>
-                <li>Machine Temperature: {machineStats.Machine_Temperature_C} °C</li>
-                <li>Soundwave dB: {machineStats.Soundwave_dB}</li>
-                <li>Vibration mm/s^2: {machineStats.Vibration_mm_s2}</li>
-                <li>Environment Temperature: {machineStats.Environment_Temperature_C} °C</li>
-                <li>Humidity: {machineStats.Humidity}%</li>
-            </ul>
-            <div>
-                <Line data={chartData} />
-            </div>
+            {machineStats.length > 0 && (
+                <ul>
+                    <li>Machine ID: {machineStats[currentIndex].Machine_ID}</li>
+                    <li>Timestamp: {machineStats[currentIndex].Timestamp}</li>
+                    <li>Temperature: {machineStats[currentIndex].Machine_Temperature_C} °C</li>
+                    <li>Sound Waves: {machineStats[currentIndex].Soundwave_dB}</li>
+                    <li>Vibration: {machineStats[currentIndex].Vibration_mm_s2}</li>
+                    <li>Environment Temperature: {machineStats[currentIndex].Environment_Temperature_C} °C</li>
+                    <li>Humidity: {machineStats[currentIndex].Humidity}%</li>
+                </ul>
+            )}
         </div>
     );
-}
+};
+
+export default Dash;
